61b00326e5387e72b18690f732d31ab5555b0709
NathanB09 <nathanbell09@gmail.com>
2018-11-21 17:42:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    @price = price
+    @price = price * quantity
     quantity.times {@items << title}
   end
 
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -79,5 +79,10 @@ describe 'CashRegister' do
       cash_register.add_item("tomato", 1.76)
       expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(1.76).to(0.0)
     end
+
+    it 'subtracts the last transaction from the total' do
+      cash_register.add_item("tomato", 1.76, 3)
+      expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(5.28).to(0.0)
+    end
   end
 end
